326c09eb9f57571ac59dc2222d1a6d372f7eb724
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-06-24 10:11:49 -0300

    implementado codigo js para o redirecionamento das paginas

---
+++ js/telaDeCheckin/checkinScreen.js
@@ -0,0 +1,9 @@
+// Description: This script handles the functionality of the check-in screen, including form submission and navigation.
+// Aguardando a implementação do banco de dados para o check-in.
+
+const btnSubmit = document.querySelector('.button-container')
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault() 
+    window.location.href = '../../Html/FormularioCheckin/form.html'
+})
