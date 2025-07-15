7c09497b05907bf9be5369a9c62891c5586c3088
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-07-15 09:17:43 -0300

    Iniciando a parte do check in

--- js/checkin/checkinScreen.js
+++ js/checkin/checkinScreen.js
@@ -4,6 +4,18 @@
 const btnSubmit = document.querySelector('.button-container')
 
 btnSubmit.addEventListener('click', (e) => {
-    e.preventDefault() 
+    e.preventDefault()
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    const cpfDigitado = document.getElementById("cpf").value.trim();
+    if (!userData.cpf) {
+        alert("Nenhum dado de usuário encontrado. Por favor, preencha o formulário de registro primeiro.");
+        return;
+    }
+    
+
+    //Logica simples, depois tenho que alterar pra procurar em todos os dados cadastrados usando o find
+    if(cpfDigitado == userData.cpf) {
+        alert("Check-in realizado com sucesso!");
+    };
     window.location.href = '/html/confirmacao-consulta/appointmentConfirmationScreen.html';
 })
