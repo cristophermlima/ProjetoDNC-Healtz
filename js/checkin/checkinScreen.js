bd1a322176b128e3f02993c671b2687bddb68546
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-07-16 18:04:24 -0300

    fix:Arrumando rota

--- js/checkin/checkinScreen.js
+++ js/checkin/checkinScreen.js
@@ -5,17 +5,6 @@ const btnSubmit = document.querySelector('.button-container')
 
 btnSubmit.addEventListener('click', (e) => {
     e.preventDefault()
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    const cpfDigitado = document.getElementById("cpf").value.trim();
-    if (!userData.cpf) {
-        alert("Nenhum dado de usuário encontrado. Por favor, preencha o formulário de registro primeiro.");
-        return;
-    }
     
-
-    //Logica simples, depois tenho que alterar pra procurar em todos os dados cadastrados usando o find
-    if(cpfDigitado == userData.cpf) {
-        alert("Check-in realizado com sucesso!");
-    };
-    window.location.href = '/html/confirmacao-consulta/appointmentConfirmationScreen.html';
+    window.location.href = '../../html/confirmacao-consulta/appointmentConfirmationScreen.html';
 })
