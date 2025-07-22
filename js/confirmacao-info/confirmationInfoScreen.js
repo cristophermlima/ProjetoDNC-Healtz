10eb6377ae50340edb2c039cebbda2e05cef37f8
kelvinjp1999 <87286523+kelvinjp1999@users.noreply.github.com>
2025-07-22 10:10:10 -0300

    enviando dados para o sheetMonkey

--- js/confirmacao-info/confirmationInfoScreen.js
+++ js/confirmacao-info/confirmationInfoScreen.js
@@ -1,6 +1,9 @@
 
 const btnConfirmation = document.querySelector('button[type="submit"]');
 const changeInfoButton = document.getElementById('changeInformation');
+const spinner = btnConfirmation.querySelector('.spinner');
+spinner.style.display = 'none';
+
 
 // function to get form Values
 function getAndSetValues() {
@@ -16,7 +19,7 @@ function getAndSetValues() {
     const cpf = document.getElementById("cpf");
     const name = document.getElementById("name");
     const age = document.getElementById("age");
-    const healthPlan = document.getElementById("heath-plan");
+    const healthPlan = document.getElementById("health-plan");
     const planNumber = document.getElementById("plan-number");
     const email = document.getElementById("email");
     const phone = document.getElementById("phone");
@@ -42,11 +45,50 @@ function getAndSetValues() {
     city.value = lastPatient.city || '';
 }
 
-btnConfirmation.addEventListener('click', (e) => {
+btnConfirmation.addEventListener('click', async (e) => {
     e.preventDefault();
 
+    
+    spinner.style.display = 'inline-block';
+    btnConfirmation.disabled = true;
 
-    window.location.href = '/html/checkin/checkInScreen.html';
+    try {
+        const response = await fetch("https://api.sheetmonkey.io/form/eyi18kqUGdE19VUTVf97WV", {
+            method: "POST",
+            headers: {
+                "accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                cpf: document.getElementById("cpf").value,
+                name: document.getElementById("name").value,
+                age: document.getElementById("age").value,
+                healthPlan: document.getElementById("health-plan").value,
+                planNumber: document.getElementById("plan-number").value,
+                email: document.getElementById("email").value,
+                phone: document.getElementById("phone").value,
+                zipCode: document.getElementById("zip-code").value,
+                address: document.getElementById("address").value,
+                neighborhood: document.getElementById("neighborhood").value,
+                complement: document.getElementById("complement").value,
+                uf: document.getElementById("uf").value,
+                city: document.getElementById("city").value
+            })
+        });
+
+        if (response.ok) {
+            window.location.href = '/html/checkin/checkInScreen.html';
+        } else {
+            alert("Erro ao enviar os dados. Verifique as informações e tente novamente.");
+        }
+    } catch (error) {
+        console.error("Erro ao enviar os dados:", error);
+        alert("Erro na conexão com o servidor.");
+    } finally{
+        btnText.textContent = "Enviar";
+        spinner.hidden = true;
+        btnConfirmation.disabled = false;
+    }
 })
 
 changeInfoButton.addEventListener('click', (e) => {
